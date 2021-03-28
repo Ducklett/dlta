8fd7a7e267caefed51123b49159a804595fc1c7f
Ducklett <ivy16203@gmail.com>
2021-03-28 18:54:38 +0200

    get basic window icon going
    doesn't actually work for title bar 🤔😔

--- Dlta/Core/Application.h
+++ Dlta/Core/Application.h
@@ -36,9 +36,12 @@ namespace dlta {
 			glfwSetErrorCallback(glfw_error_callback);
 
 			glfwInit();
+
+
 			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 			glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 			//glfwWindowHint(GLFW_SAMPLES, 4);
 
 #ifdef __APPLE
